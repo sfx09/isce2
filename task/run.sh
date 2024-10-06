d82ec92caa3e6b773b11e050610110d006f27f91
sfx09 <aleth@tuta.io>
2024-10-06 20:40:57 +0530

    dynamically query SLC list

--- task/run.sh
+++ task/run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-source env.sh
-source .venv/bin/activate
-
 set -e 
+
+source env.sh
 
 mkdir -p artifacts/{dem,slc,orbits,aux}
 
 # download SLC files
+SLC_LINK="https://korraicom-my.sharepoint.com/:t:/g/personal/rahul_korrai_com/EbnUgoKGx51JjfFA9Q9N6UwB9bowJox52oPs1AgUVh8Fzg?download=1"
+wget "$SLC_LINK" -O artifacts/slc/SLC_LIST.txt
 python3 slc.py
 
 cd artifacts
@@ -16,7 +17,8 @@ cd artifacts
 dloadOrbits.py -d orbits
 
 # downloading AUX files might not be required, since SLCs are dated after 2015. (no error-correction needed)
-wget https://sar-mpc.eu/download/ca97845e-1314-4817-91d8-f39afbeff74d/ -O aux/S1A_AUX_CAL_V20140908T000000_G20190626T100201.SAFE.zip
+AUX_LINK="https://sar-mpc.eu/download/ca97845e-1314-4817-91d8-f39afbeff74d/"
+wget "$AUX_LINK" -O aux/S1A_AUX_CAL_V20140908T000000_G20190626T100201.SAFE.zip
 
 # download DEM
 cd dem
